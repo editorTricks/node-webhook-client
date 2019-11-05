a4dd0caf32cdd8ad2f5170769c12ad47978ed09a
Editor Tricks <57374539+editorTricks@users.noreply.github.com>
2019-11-05 06:23:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({
  * @type {WeeblyMiddleware|exports|module.exports}
  */
 const wMiddleware = new WeeblyMiddleware({
-	'client_id': '',
+	'client_id': '690460479',
 	'secret_key': '5e9ff66338b647c4ff03dc55be50536e4c02f2de57f6b2b45cc9d0ac587c2bef'
 });
 
